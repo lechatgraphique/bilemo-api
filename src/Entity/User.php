140fc578d7242485737f2772cf90b51ba48004cd
Bastien CLEMENT <bclement@assurone-group.com>
2020-08-22 15:19:39 +0200

    Entities Relations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -42,16 +44,38 @@ class User
      */
     private ?string $password = null;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Client::class, mappedBy="user")
+     * @var Client|null
+     */
+    private ?Client $clients = null;
+
+
+    public function __construct()
+    {
+        $this->clients = new ArrayCollection();
+    }
+
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getUsername(): ?string
     {
         return $this->username;
     }
 
+    /**
+     * @param string|null $username
+     * @return $this
+     */
     public function setUsername(?string $username): self
     {
         $this->username = $username;
@@ -59,11 +83,18 @@ class User
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getEmail(): ?string
     {
         return $this->email;
     }
 
+    /**
+     * @param string|null $email
+     * @return $this
+     */
     public function setEmail(?string $email): self
     {
         $this->email = $email;
@@ -76,6 +107,10 @@ class User
         return $this->roles;
     }
 
+    /**
+     * @param string|null $roles
+     * @return $this
+     */
     public function setRoles(?string $roles): self
     {
         $this->roles = $roles;
@@ -83,14 +118,57 @@ class User
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getPassword(): ?string
     {
         return $this->password;
     }
 
+    /**
+     * @param string|null $password
+     * @return $this
+     */
     public function setPassword(?string $password): self
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Client[]
+     */
+    public function getClients(): Collection
+    {
+        return $this->clients;
+    }
+
+    /**
+     * @param Client $client
+     * @return $this
+     */
+    public function addClient(Client $client): self
+    {
+        if (!$this->clients->contains($client)) {
+            $this->clients[] = $client;
+            $client->addUser($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param Client $client
+     * @return $this
+     */
+    public function removeClient(Client $client): self
+    {
+        if ($this->clients->contains($client)) {
+            $this->clients->removeElement($client);
+            $client->removeUser($this);
+        }
 
         return $this;
     }
